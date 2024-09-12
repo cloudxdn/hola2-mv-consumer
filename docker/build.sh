0dbab74ec0d2b11a8ccd6e4c76806b0a3d83c6bd
Kim-KiBoum <rlarl7477@atlasnetworks.co.kr>
2024-09-12 14:53:38 +0900

    상용 버전

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 tar cvf hola2-mv-consumer.tar hola2-mv-consumer
-docker build -t registry.tde.sktelecom.com/sktsdn/sdn-bp-dockerfiles/hola2-mv-consumer:v2.240820.170525 -f docker/Dockerfile . --platform linux/amd64
+docker build -t registry.tde.sktelecom.com/sktsdn/sdn-bp-dockerfiles/hola2-mv-consumer:v2.240905.172600 -f docker/Dockerfile . --platform linux/amd64
